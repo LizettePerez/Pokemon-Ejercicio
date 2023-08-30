c437df9e774211916d704b2eb1dba99d3b9a2699
LizettePerez <lizette.perezcorrea@gmail.com>
2023-08-30 17:04:17 -0400

    prueba1

---
+++ src/Parte1/Main.java
@@ -0,0 +1,8 @@
+package Parte1;
+
+public class Main {
+    public static void main(String[] args) {
+        Pokemon Pikachu = new Pokemon("Pikachu", 25, "Electrico", null);
+        System.out.println(Pikachu);
+    }
+}
